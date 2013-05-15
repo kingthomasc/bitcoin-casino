c9d484e65af84affd9ce4ef55c8e442045e41bab
Tom King <tomk@bixly.com>
2013-05-14 19:23:11 -0700

    fix to wsgi file

--- casino/views.py
+++ casino/views.py
@@ -3,6 +3,7 @@ Created on May 9, 2013
 
 @author: Thomas
 '''
+from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html', {})
--- casino/wsgi.py
+++ casino/wsgi.py
@@ -14,12 +14,14 @@ framework.
 
 """
 import os
+import sys
 
 # We defer to a DJANGO_SETTINGS_MODULE already in the environment. This breaks
 # if running multiple sites in the same mod_wsgi process. To fix this, use
 # mod_wsgi daemon mode with each site in its own daemon process, or use
-# os.environ["DJANGO_SETTINGS_MODULE"] = "casino.settings"
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "casino.settings")
+sys.path.append('/opt/webapps/cryptoption/bitcoin-casino')
+os.environ["DJANGO_SETTINGS_MODULE"] = "casino.settings"
+# os.environ.setdefault("DJANGO_SETTINGS_MODULE", "casino.settings")
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
